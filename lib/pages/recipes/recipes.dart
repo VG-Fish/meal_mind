faf1874ba5d8f07d43c0a54aa39d15f801a0bc1c
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2025-06-21 17:01:23 -0700

    moved recipe search bar into project components folder

--- lib/pages/recipes/recipes.dart
+++ lib/pages/recipes/recipes.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-import 'components/search.dart';
+import '../components/recipe_search_bar.dart';
 
 import 'dart:convert';
 
